c0fd9764de2dad190aeb938961b89b124d3bde8d
Kazuki Kanaya Desktop <k.melon0106@gmail.com>
2025-06-15 03:23:50 +0900

    不要なファイルを削除

--- test.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-
-mlflow.set_tracking_uri("file:artifacts/mlflow")
-mlflow.set_experiment("Default")  # experiment名が一致していればOK
-
-with mlflow.start_run(run_name="demo-run"):
-    mlflow.log_param("learning_rate", 0.01)
-    mlflow.log_metric("val_accuracy", 0.92)
